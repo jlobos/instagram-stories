571ed6aa29a0e07bfdd989d76ab903a91947e0fd
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-02-24 19:58:52 +0530

    test.js update

--- test.js
+++ test.js
@@ -40,7 +40,7 @@ test('getStoriesFeed', async t => {
 })
 
 test('getMediaByLocation', async t => {
-  const {locationId} = await getMediaByLocation('292188415')
+  const {location} = await getMediaByLocation('292188415')
   t.is(location.location.id, '292188415')
   t.is(location.location.username, 'Eiffel Tower')
   t.is(location.location.slug, 'eiffel-tower')
